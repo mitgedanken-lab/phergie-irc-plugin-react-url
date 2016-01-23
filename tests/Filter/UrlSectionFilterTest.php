637af134215ac0bfc1b605085aa32dd1cc84aa13
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-01-23 23:44:14 +0100

    Event and filter for url's

---
+++ tests/Filter/UrlSectionFilterTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Phergie\Irc\Plugin\React\Url\Filter;
+
+use Phake;
+use Phergie\Irc\Event\EventInterface;
+
+class UrlSectionFilterTest extends \PHPUnit_Framework_TestCase
+{
+    public function provideFilter()
+    {
+        yield [
+            'host',
+            'phergie.org',
+            'http://phergie.org/',
+            true,
+        ];
+
+        yield [
+            'host',
+            'phergie.org',
+            'http://www.phergie.org/',
+            false,
+        ];
+
+        yield [
+            'host',
+            '*.org',
+            'http://phergie.org/',
+            true,
+        ];
+    }
+
+    /**
+     * @dataProvider provideFilter
+     */
+    public function testFilter($section, $value, $url, $output)
+    {
+        $event = Phake::mock(EventInterface::class);
+        $urlEvent = new UrlEvent($url, $event);
+        $filter = new UrlSectionFilter($section, $value);
+        $this->assertSame($output, $filter->filter($urlEvent));
+    }
+}
